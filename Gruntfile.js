c4a47cdbd3698454f301d322a89cd0ab83f43796
Alex Li <7alex7li@gmail.com>
2020-06-26 10:39:43 -0400

    Clean up eslintrc and remove old linter

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,17 +19,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    closureLint: {
-      app:{
-        closureLinterPath : 'third_party/closure-linter/closure_linter',
-        src: ['src/**/*.js'],
-        options: {
-          stdout: true,
-          strict: true
-        }
-      }
-    },
-
     closureDepsWriter: {
       options: {
         closureLibraryPath: 'third_party/closure-library',
@@ -64,11 +53,9 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-closure-tools');
-  grunt.loadNpmTasks('grunt-closure-linter');
   grunt.loadNpmTasks('grunt-contrib-qunit');
 
   grunt.registerTask('default', [
-    'closureLint',
     'closureDepsWriter',
     'closureBuilder',
     'qunit'
