030aa16f82c989c65f9228c04d9ef46d3e52fe41
Pedro Silva <pedro.hs.silva0111@gmail.com>
2020-06-30 12:49:29 -0500

    added appropriate tests for argument support

--- test/isArgument_test.js
+++ test/isArgument_test.js
@@ -1,22 +1,35 @@
 goog.require('helper');
 
 describe('The helper.isArguments_ function', function() {
-
   it('Identifies an arguments object', function() {
-      const argumentsTest = () => {
-        expect(helper.isArguments_(arguments)).toBe(true);
-      };
-      argumentsTest(1, 2, 3, 4);
+    const argumentsTest = () => {
+      expect(helper.isArguments_(arguments)).toBe(true);
+    };
+    argumentsTest(1, 2, 3, 4);
   });
 
-  it('Identifies an Arguments object when it is an argument itself', function() {
-      const argumentsTest = () => {
-          argumentsAsArgTest(arguments);
-      }
-      const argumentsAsArgTest = (args) => {
-        expect(helper.isArguments_(args)).toBe(true);
-      };
-      argumentsTest(1, 2, 3, 4);
+  it('Identifies an empty arguments object', function() {
+    const argumentsTest = () => {
+      expect(helper.isArguments_(arguments)).toBe(true);
+    };
+    argumentsTest();
+  });
+
+  it('Identifies Arguments object when it is an argument itself', function() {
+    const argumentsTest = () => {
+      argumentsAsArgTest(arguments);
+    };
+    const argumentsAsArgTest = (args) => {
+      expect(helper.isArguments_(args)).toBe(true);
+    };
+    argumentsTest(1, 2, 3, 4);
+  });
+
+  it('Identifies an rest parameters as not an arguments object', function() {
+    const restTest = (...restArgs) => {
+      expect(helper.isArguments_(...restArgs)).toBe(false);
+    };
+    restTest('foo', 2, [null, null], 4);
   });
 
   it('Identifies null as not an Arguments object', function() {
