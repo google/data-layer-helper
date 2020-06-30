e34a8fdc3b2dc0edede3b5ae08d17aabd111ab29
KJ Galvan <kaityagalvan@gmail.com>
2020-06-30 11:23:28 -0700

    stay in line with style guide

--- test/expandKeyValue_test.js
+++ test/expandKeyValue_test.js
@@ -1,13 +1,23 @@
 goog.require('helper');
 
-describe('The helper.expandKeyValue_ function', () => {
+describe('The helper.expandKeyValue_ function', function() {
+  /**
+   * Tests the expandKeyValue function by passing in a key value pair
+   * and comparing the return value to an expected return value.
+   * @param {string} key The key's path, where dots are the path separators.
+   * @param {*} value The value to set on the given key path.
+   * @param {Object} expected An object representing the given key/value which
+   *     can be merged onto the dataLayer's model.
+   * @param {string=} context More information about the test being ran.
+   */
   function assertExpand(key, value, expected, context = '') {
     expect(helper.expandKeyValue_(key, value))
         .withContext(context)
         .toEqual(expected);
   }
 
-  it('successfully converts key value pairs into objects when the key string is non dot-separated', () => {
+  it('successfully converts key value pairs ' +
+      'into objects when the key string is non dot-separated', function() {
     assertExpand('a', 1, {a: 1});
     assertExpand('a', 0, {a: 0});
     assertExpand('a', -1, {a: -1});
@@ -20,7 +30,8 @@ describe('The helper.expandKeyValue_ function', () => {
     assertExpand('a', {b: {c: 3, d: 4}}, {a: {b: {c: 3, d: 4}}});
   });
 
-  it('successfully converts key value pairs into nested objects when the key string is dot-separated', () => {
+  it('successfully converts key value pairs into nested objects ' +
+      'when the key string is dot-separated', function() {
     assertExpand('a.b', 2, {a: {b: 2}});
     assertExpand('a.b', 0, {a: {b: 0}});
     assertExpand('a.b', -2, {a: {b: -2}});
@@ -35,13 +46,24 @@ describe('The helper.expandKeyValue_ function', () => {
     assertExpand('a.b.c.d.e.f', 6, {a: {b: {c: {d: {e: {f: 6}}}}}});
   });
 
-  it('correctly handles empty strings', () => {
-    assertExpand('', 1, {'': 1}, 'preserves explicit empty strings for keys');
-    assertExpand('a', '', {a: ''}, 'preserves explicit empty strings for values');
-    assertExpand('a..b', 2, {a: {'': {b: 2}}}, 'interprets a key with no characters between dots as being an empty string');
+  it('correctly handles empty strings', function() {
+    assertExpand(
+        '', 1, {'': 1},
+        'preserves explicit empty strings for keys'
+    );
+    assertExpand(
+        'a', '', {a: ''},
+        'preserves explicit empty strings for values'
+    );
+    assertExpand(
+        'a..b', 2, {a: {'': {b: 2}}},
+        'interprets a key with no characters between dots ' +
+            'as being an empty string'
+    );
   });
 
-  it('does not interpret dot separated keys in value objects as object nesting', () => {
+  it('does not interpret dot separated keys in value objects ' +
+      'as object nesting', function() {
     assertExpand('a', {'b.c': 1}, {a: {'b.c': 1}});
     assertExpand('a.b', {'b.c': 1}, {a: {b: {'b.c': 1}}});
   });
