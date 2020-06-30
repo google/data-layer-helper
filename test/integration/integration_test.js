d2e7a80ff263eb396ad1e5f92ecda41690a9f990
Alex Li <7alex7li@gmail.com>
2020-06-30 13:14:17 -0400

    I think that using 'this' is a more messy than helpful so I removed it

--- test/integration/integration_test.js
+++ test/integration/integration_test.js
@@ -9,8 +9,9 @@ describe('The data layer helper library', function() {
   });
 
   describe('The DataLayerHelper API', function() {
+    let helper;
     beforeEach(function() {
-      this.helper = new DataLayerHelper([]);
+      helper = new DataLayerHelper([]);
     });
 
     it('exposes DataLayerHelper constructor', function() {
@@ -18,20 +19,22 @@ describe('The data layer helper library', function() {
     });
 
     it('has two functions, get and flatten', function() {
-      expect(typeof this.helper.get).toBe('function');
-      expect(typeof this.helper.flatten).toBe('function');
+      expect(typeof helper.get).toBe('function');
+      expect(typeof helper.flatten).toBe('function');
     });
 
     it('does not expose any private helper functions', function() {
-      expect(this.helper.processStates_).toBe(undefined);
-      expect(this.helper.expandKeyValue_).toBe(undefined);
-      expect(this.helper.isArray_).toBe(undefined);
-      expect(this.helper.merge_).toBe(undefined);
+      expect(helper.processStates_).toBe(undefined);
+      expect(helper.expandKeyValue_).toBe(undefined);
+      expect(helper.isArray_).toBe(undefined);
+      expect(helper.merge_).toBe(undefined);
     });
   });
 
   describe('The functionality of the helper', function() {
     let callbacks;
+    let dataLayer;
+    let helper;
 
     /**
      * Assert that the last callback is what we expect and the number of
@@ -72,119 +75,119 @@ describe('The data layer helper library', function() {
 
     beforeEach(function() {
       callbacks = [];
-      this.dataLayer = [];
-      this.helper = new DataLayerHelper(this.dataLayer, callbackListener);
+      dataLayer = [];
+      helper = new DataLayerHelper(dataLayer, callbackListener);
     });
 
     describe('The result of calling built in methods', function() {
       it(`returns undefined for data that hasn't been pushed`, function() {
         expect(callbacks.length).toBe(0);
-        expect(this.helper.get('one')).toBe(undefined);
-        expect(this.helper.get('two')).toBe(undefined);
+        expect(helper.get('one')).toBe(undefined);
+        expect(helper.get('two')).toBe(undefined);
       });
 
       it('can set two objects in the abstract state in one data layer push',
           function() {
-            this.dataLayer.push({one: 1, two: 2});
+            dataLayer.push({one: 1, two: 2});
 
             assertCallback([{one: 1, two: 2}, {one: 1, two: 2}], 1);
 
-            expect(this.helper.get('one')).toBe(1);
-            expect(this.helper.get('two')).toBe(2);
+            expect(helper.get('one')).toBe(1);
+            expect(helper.get('two')).toBe(2);
           });
 
       it('can override an object in the abstract state after data layer push',
           function() {
-            this.dataLayer.push({one: 1, two: 2}, {two: 3});
+            dataLayer.push({one: 1, two: 2}, {two: 3});
 
             assertCallback([{one: 1, two: 3}, {two: 3}], 2);
 
-            expect(this.helper.get('one')).toBe(1);
-            expect(this.helper.get('two')).toBe(3);
+            expect(helper.get('one')).toBe(1);
+            expect(helper.get('two')).toBe(3);
           });
 
       it('can overwrite data twice in a row', function() {
-        this.dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
+        dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
 
         assertCallback([{one: 1, two: 2}, {two: 2}], 3);
 
-        expect(this.helper.get('one')).toBe(1);
-        expect(this.helper.get('two')).toBe(2);
+        expect(helper.get('one')).toBe(1);
+        expect(helper.get('two')).toBe(2);
       });
 
       it('can write to deeply nested objects', function() {
-        this.dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
-        this.dataLayer.push({one: {three: 3}});
+        dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
+        dataLayer.push({one: {three: 3}});
 
         assertCallback([{one: {three: 3}, two: 2}, {one: {three: 3}}], 4);
 
-        expect(this.helper.get('one')).toEqual({three: 3});
-        expect(this.helper.get('two')).toBe(2);
+        expect(helper.get('one')).toEqual({three: 3});
+        expect(helper.get('two')).toBe(2);
       });
 
       it('can append to deeply nested objects', function() {
-        this.dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
-        this.dataLayer.push({one: {three: 3}}, {one: {four: 4}});
+        dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
+        dataLayer.push({one: {three: 3}}, {one: {four: 4}});
 
         assertCallback([
           {one: {three: 3, four: 4}, two: 2},
           {one: {four: 4}}], 5);
 
-        expect(this.helper.get('one')).toEqual({three: 3, four: 4});
-        expect(this.helper.get('one.four')).toBe(4);
-        expect(this.helper.get('two')).toBe(2);
+        expect(helper.get('one')).toEqual({three: 3, four: 4});
+        expect(helper.get('one.four')).toBe(4);
+        expect(helper.get('two')).toBe(2);
       });
 
       it('can flatten the history to a single object', function() {
-        this.dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
-        this.dataLayer.push({one: {three: 3}}, {one: {four: 4}});
+        dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
+        dataLayer.push({one: {three: 3}}, {one: {four: 4}});
 
-        expect(this.dataLayer.length).toBe(5);
-        expectEqualContents(this.dataLayer, [
+        expect(dataLayer.length).toBe(5);
+        expectEqualContents(dataLayer, [
           {one: 1, two: 2}, {two: 3},
           {two: 2}, {one: {three: 3}}, {one: {four: 4}}]);
 
-        this.helper.flatten();
+        helper.flatten();
 
-        expect(this.dataLayer.length).toBe(1);
-        expectEqualContents(this.dataLayer,
+        expect(dataLayer.length).toBe(1);
+        expectEqualContents(dataLayer,
             [{one: {three: 3, four: 4}, two: 2}]);
 
-        expect(this.helper.get('one')).toEqual({three: 3, four: 4});
-        expect(this.helper.get('one.four')).toBe(4);
-        expect(this.helper.get('two')).toBe(2);
+        expect(helper.get('one')).toEqual({three: 3, four: 4});
+        expect(helper.get('one.four')).toBe(4);
+        expect(helper.get('two')).toBe(2);
       });
 
       it('works normally after flatten', function() {
-        this.dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
-        this.dataLayer.push({one: {three: 3}}, {one: {four: 4}});
-        this.helper.flatten();
-        this.dataLayer.push({five: 5});
+        dataLayer.push({one: 1, two: 2}, {two: 3}, {two: 2});
+        dataLayer.push({one: {three: 3}}, {one: {four: 4}});
+        helper.flatten();
+        dataLayer.push({five: 5});
 
         assertCallback([
           {one: {three: 3, four: 4}, two: 2, five: 5}, {five: 5}], 6);
 
-        expect(this.dataLayer.length).toBe(2);
-        expect(this.helper.get('one.four')).toBe(4);
-        expect(this.helper.get('five')).toBe(5);
+        expect(dataLayer.length).toBe(2);
+        expect(helper.get('one.four')).toBe(4);
+        expect(helper.get('five')).toBe(5);
       });
     });
 
     describe('The result of calling custom methods', function() {
       it('calls custom methods pushed to the dataLayer that ' +
         'may change state', function() {
-        this.dataLayer.push(
+        dataLayer.push(
             {a: 'originalValue'},
             () => {
-              this.dataLayer.push({a: 'newValue'});
+              dataLayer.push({a: 'newValue'});
             });
 
-        expect(this.helper.get('a')).toBe('newValue');
+        expect(helper.get('a')).toBe('newValue');
       });
 
       it('Allows for recursive type methods that ' +
         'push themselves to the dataLayer', function() {
-        const dataLayer = this.dataLayer;
+        const dataLayer = dataLayer;
         dataLayer.push(
             {numCustomMethodCalls: 0},
             function() {
@@ -198,7 +201,7 @@ describe('The data layer helper library', function() {
               dataLayer.push(method);
             });
 
-        expect(this.helper.get('numCustomMethodCalls')).toBe(10);
+        expect(helper.get('numCustomMethodCalls')).toBe(10);
       });
     });
   });
