20e4938f43de80a714fef9085d98b1236b2257e8
Alex Li <7alex7li@gmail.com>
2020-07-06 18:05:15 -0400

    Move tests for the flatten method

---
+++ test/flatten_test.js
@@ -0,0 +1,83 @@
+/**
+ * @fileoverview Unit tests for flatten().
+ */
+goog.module('datalayerhelper.helper.testing.flatten');
+goog.setTestOnly();
+
+const {DataLayerHelper} = goog.require('helper');
+const {expectEqualContents} = goog.require('datalayerhelper.helper.testing.utils');
+describe(`The 'flatten' method of helper`, function() {
+  beforeEach(function() {
+    this.dataLayer = [{a: 1, b: {c: {d: 4}, e: 5}}];
+    this.dataLayer.push({f: 6});
+    this.dataLayer.push({g: 7});
+
+    this.h = new DataLayerHelper(this.dataLayer);
+    this.dataLayer.push({g: 8, i: 9});
+    this.dataLayer.push({'b.c': 3});
+  });
+
+  it('compresses a complex history into 1 call', function() {
+    expectEqualContents([
+      {a: 1, b: {c: {d: 4}, e: 5}},
+      {f: 6},
+      {g: 7},
+      {g: 8, i: 9},
+      {'b.c': 3},
+    ], this.dataLayer);
+
+    this.h.flatten();
+
+    expectEqualContents([{
+      a: 1,
+      b: {c: 3, e: 5},
+      f: 6,
+      g: 8,
+      i: 9,
+    }], this.dataLayer);
+  });
+
+  it('does nothing extra when called twice in a row', function() {
+    this.h.flatten();
+    this.h.flatten();
+
+    expectEqualContents([
+      {
+        a: 1,
+        b: {c: 3, e: 5},
+        f: 6,
+        g: 8,
+        i: 9,
+      }], this.dataLayer);
+  });
+
+  it('works properly when overwriting older values', function() {
+    this.h.flatten();
+    this.dataLayer.push({f: 7, j: 10});
+
+    expectEqualContents([
+      {
+        a: 1,
+        b: {c: 3, e: 5},
+        f: 6,
+        g: 8,
+        i: 9,
+      },
+      {
+        f: 7,
+        j: 10,
+      },
+    ], this.dataLayer);
+
+    this.h.flatten();
+
+    expectEqualContents([{
+      a: 1,
+      b: {c: 3, e: 5},
+      f: 7,
+      g: 8,
+      i: 9,
+      j: 10,
+    }], this.dataLayer);
+  });
+});
---
+++ test/utils.js
@@ -0,0 +1,23 @@
+goog.module('datalayerhelper.helper.testing.utils');
+goog.setTestOnly();
+
+/**
+ * Checks that each of the values of two array-like objects are deep equal.
+ * This is needed because jasmine's toEqual compares the functional
+ * attributes of dataLayer, which includes a push function.
+ * Since the push function is an anonymous function that gets
+ * recreated every time, we can't create an object that
+ * is deep equal to any dataLayer.
+ * @param {!Array<Object>} arr1 The first array-like object
+ * @param {!Array<Object>} arr2 The second array-like object
+ */
+function expectEqualContents(arr1, arr2) {
+  expect(arr1.length).toBe(arr2.length);
+  for (let i = 0; i < arr1.length; i++) {
+    expect(arr1[i]).toEqual(arr2[i]);
+  }
+}
+
+exports = {
+  expectEqualContents,
+};
