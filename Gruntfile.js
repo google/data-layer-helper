6584ce433d318b11ed7e7b03bf64c9cc19533c63
Alex Li <7alex7li@gmail.com>
2020-07-08 15:50:41 -0400

    Check out winows again and also see if some lines are needed

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,8 +73,8 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', [
     'closureDepsWriter',
-//    'closure-compiler',
+    'closure-compiler',
     'karma:unit',
-//    'karma:integration',
+    'karma:integration',
   ]);
 };
